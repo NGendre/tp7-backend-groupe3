31a731e8dd9efe63acc9928f24439834972be60d
NGendre <noe.gendre@outlook.fr>
2023-06-02 11:20:18 +0200

    fix: unit tests admin item router + refactor admin item files

--- __tests__/routers/test_adminItemRouterTests.py
+++ __tests__/routers/test_adminItemRouterTests.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 from src.models.ItemModel import ItemModel
-from src.routers.admin.items.AdminItemRouter import addItem, updateOne
+from src.routers.admin.items.AdminItemRouter import updateOne, getOne, addItem, deleteOne, getAll
 from src.schemas.ItemSchema import ItemSchema
 from src.services.ItemService import ItemService
 
@@ -12,30 +12,32 @@ class ItemRouterTests(unittest.TestCase):
         self.item_schema = ItemSchema(libobj = 'test',tailleobj = 'grand',puobj = 11.1,poidsobj = 22.2)
 
     def test_add_item(self):
-        self.item_service_mock.addItem.return_value = self.item_schema
+        self.item_service_mock.create.return_value = self.item_schema
         addOneUser = addItem(self.item_schema,self.item_service_mock)
-        self.item_service_mock.assert_called_once_with(self.item_schema)
+        self.item_service_mock.create.assert_called_once_with(self.item_schema)
         self.assertEquals(self.item_schema,addOneUser)
 
     def test_get_one(self):
-        getOneUser = self.item_service_mock.getOne(item_service_mock=self.item_service_mock)
-        self.assertEquals(getOneUser, self.item_schema)
-        self.item_service_mock.assert_called_once()
+        self.item_service_mock.getOne.return_value = self.item_schema
+        getOneUser = getOne(1,self.item_service_mock)
+        self.assertEqual(getOneUser, self.item_schema)
+        self.item_service_mock.getOne.assert_called_once_with(1)
 
     def test_get_all(self):
-        getAllUsers = self.item_service_mock.getAll(item_service_mock = self.item_service_mock)
+        self.item_service_mock.getAll.return_value = self.item_schema
+        getAllUsers = getAll(self.item_service_mock)
         self.assertEquals(getAllUsers,self.item_schema)
-        self.item_service_mock.assert_called_once()
+        self.item_service_mock.getAll.assert_called_once()
 
     def test_update_one(self):
         self.item_service_mock.update.return_value = self.item_schema
-        updateOneUser = updateOne(self.item_schema,self.item_service_mock)
-        self.item_service_mock.assert_called_once_with(self.item_schema)
+        updateOneUser = updateOne(1,self.item_schema,self.item_service_mock)
+        self.item_service_mock.update.assert_called_once_with(1,self.item_schema)
         self.assertEquals(self.item_schema,updateOneUser)
 
 
     def test_delete_one(self):
-        ItemService.delete(1)
+        deleteOne(1,self.item_service_mock)
         self.item_service_mock.delete.assert_called_once_with(1)
 
 
